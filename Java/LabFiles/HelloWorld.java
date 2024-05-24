6852e65a48a0546461ca1496dbe98c18e6315117
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-05-24 20:09:08 +0530

    Create HelloWorld.java

---
+++ Java/LabFiles/HelloWorld.java
@@ -0,0 +1,11 @@
+package LabFiles;
+/*
+ * Date: 2023-08-07 (YYYY-MM-DD)
+ * Program: Hello world program
+*/
+
+public class HelloWorld {
+    static public void main(String[] input) {
+        System.out.println("Hello, World!");
+    }
+}
