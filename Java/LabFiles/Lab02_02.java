c1db6d4e6ab951c139c1e071a3c717331f75e42d
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-05-24 20:14:00 +0530

    Create Lab02_02.java

---
+++ Java/LabFiles/Lab02_02.java
@@ -0,0 +1,19 @@
+package LabFiles;
+
+public class Lab02_02 {
+    public static void main(String[] args) {
+        animal a1 = new animal();
+        a1.show();
+    }
+}
+
+class animal {
+    String s = "DOG"; // instance variable
+    // static String s = "DOG"; // static variable, only occupies memory once
+
+    void show() {
+        String s = "CAT"; // local variable
+        System.out.println(s);
+        System.out.println(this.s);
+    }
+}
