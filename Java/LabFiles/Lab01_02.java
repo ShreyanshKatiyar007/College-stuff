871a35c5e9ceb4febe0af42c9e61385491f88061
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-05-24 20:11:34 +0530

    Create Lab01_02.java

---
+++ Java/LabFiles/Lab01_02.java
@@ -0,0 +1,42 @@
+package LabFiles;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Lab01_02 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter size of array:");
+        int size = in.nextInt();
+
+        int[] arr = new int[size];
+        arr = new int[50];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        // print elements
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("Enter position to enter new element: ");
+        int position = in.nextInt();
+        System.out.println("Enter value to insert: ");
+        int value = in.nextInt();
+
+        for (int i = size - 1; i >= position - 1; i--) {
+            arr[i + 1] = arr[i];
+        }
+        arr[position - 1] = value;
+
+        System.out.println("New array:");
+        for (int i = 0; i < size + 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
