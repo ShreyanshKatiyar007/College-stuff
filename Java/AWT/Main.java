40cc052eaa5a0968fa7f479856a165a6bcf0312d
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-05-24 20:07:43 +0530

    Create Main.java

---
+++ Java/AWT/Main.java
@@ -0,0 +1,13 @@
+package AWT;
+
+import java.awt.*;
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        Frame f=new Frame();
+
+        f.setSize(900,600);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
